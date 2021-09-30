8045a4ceac443ca136d983b466562e530250e799
jackie <jackielin7789978@gmail.com>
2021-09-30 23:36:26 +0800

    FP-44 common.js 增加註解

--- src/components/common.js
+++ src/components/common.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+// 撐主頁面用，已加在 Route 外面，單一頁面不用再套
 export const PageContainer = styled.div`
   margin-top: 120px;
   flex: 1;
 `;
 
+// 可包在圖片裡的連結 component，height 需視圖片高度另外加上去
 export const ImgAnchor = styled(Link)`
   display: inline-block;
   width: 100%;
