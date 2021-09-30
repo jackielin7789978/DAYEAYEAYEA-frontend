c01b3b32ff8e9a1cfd782924141cd75d80e12ae0
jackie <jackielin7789978@gmail.com>
2021-09-30 16:35:46 +0800

    FP-44 添加共用顏色

--- src/constants/style.js
+++ src/constants/style.js
@@ -1,13 +1,32 @@
 export const COLOR = {
-  navbar: "rgb(210, 201, 177)",
-  navbar_link_hover: "rgb(196, 157, 124)",
-  footer: "rgb(154, 139, 102)",
-  box_shadow: "rgba(100, 100, 100, 0.2)",
-  background_light: "#fff",
+  primary_light: "#D2C9B1",
+  primary_dark: "#9A8B66",
+  warning: "#FFA100",
+  accent: "#5B756E",
+  light: "#FFF",
+
   text_light: "#fff",
   text_dark: "#333",
+  text_grey: "#999",
+
+  border_primary: "#D2C9B1",
+  border_grey: "#999",
+  border_light: "#fff",
+  border_dark: "#333",
 };
 
 export const FONT = {
   logo: "'Karla', sans-serif;",
+};
+
+export const FONT_SIZE = {
+  logo: "42px",
+  xl: "30px",
+  lg: "24px",
+  md: "18px",
+  sm: "14px",
+};
+
+export const EFFECT = {
+  shadow_dark: `0px 5px 10px 1px rgba(100, 100, 100, 0.2);`,
 };
