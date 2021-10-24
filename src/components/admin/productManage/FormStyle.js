d43645d754183939c3acbd67ef58807fb2a744ca
Ding <mumu892101@gmail.com>
2021-10-24 18:16:09 +0800

    add FormStyle

--- src/components/admin/productManage/FormStyle.js
+++ src/components/admin/productManage/FormStyle.js
@@ -1,124 +1,146 @@
 import styled from 'styled-components'
-import { Wrapper } from '../../../components/admin/TableStyle'
 import { ADMIN_COLOR, COLOR, FONT_SIZE } from '../../../constants/style'
+import { EditBtn, LogoutBtn, SaveBtn } from '../../../components/Button'
 
-const FormContainer = styled(Wrapper)`
-  background: ${ADMIN_COLOR.light_grey};
-  border-radius: 20px;
-  position: relative;
-  margin: 40px auto;
-  padding: 20px 40px;
-  width: 70vw;
+const FormWrapper = styled.div`
+  min-height: 900px;
+  height: 100%;
+  transition: 1.5s;
+  width: 90%;
+  margin: 30px auto;
+  border: 1px solid ${ADMIN_COLOR.border_grey};
+`
+const Form = styled.form`
+  padding: 20px 0px 50px 0px;
+  height: 100%;
+`
+const Input = styled.input`
+  display: block;
+  width: 100%;
+  padding: 10px;
+  border-radius: 3px;
+  border: solid 1px ${ADMIN_COLOR.border_grey};
+  ::placeholder {
+    color: ${COLOR.text_placeholder};
+  }
+  &:focus {
+    border: solid 1px ${ADMIN_COLOR.border_dark_grey};
+  }
+`
+const InputTitle = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-const FormTitle = styled.h4`
-  text-align: center;
-  padding: 20px;
-  font-weight: bold;
-`
-const FormInputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
-const FormInput = styled.div`
-  display: flex;
-  margin-bottom: 50px;
-  align-items: flex-start;
-  justify-content: flex-start;
-`
-
-const FormInputTitleStyle = styled.div`
   font-size: ${FONT_SIZE.md};
-  color: ${COLOR.text_dark};
-  font-weight: bold;
-  white-space: nowrap;
-  width: 15%;
+  margin-bottom: 10px;
+`
+const FormTitle = styled.div`
+  font-size: ${FONT_SIZE.lg};
+  color: ${ADMIN_COLOR.table_blue};
+  margin-bottom: 10px;
+`
+const FormTitleBorder = styled.div`
+  border-top: 1px solid ${ADMIN_COLOR.border_grey};
 `
 
-function FormInputTitle({ title }) {
-  return <FormInputTitleStyle>{title}</FormInputTitleStyle>
-}
-
-const FormInputMsgDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
-const FormInputStyle = styled.input`
-  width: 42vw;
-  padding: 5px;
-  border: 1px solid ${ADMIN_COLOR.border_grey};
-  &:focus {
-    border: 1px solid ${ADMIN_COLOR.border_dark_grey};
-  }
-  font-size: ${FONT_SIZE.sm};
-  color: ${COLOR.text_dark};
-  font-weight: bold;
-  margin-left: 30px;
-  margin-top: 2px;
-  white-space: pre-wrap;
-`
-
-function FormInputContent({ value }) {
-  return <FormInputStyle value={value} />
-}
-
-const ErrorMsgStyle = styled.div`
-  color: ${COLOR.text_warning};
+const ErrorMsg = styled.p`
+  margin-top: 5px;
+  color: ${ADMIN_COLOR.warning};
   font-size: ${FONT_SIZE.xs};
-  margin: 10px 0px 0px 30px;
-`
-
-const ProductDescTextArea = styled.textarea`
-  border: 1px solid ${ADMIN_COLOR.border_grey};
-  &:focus {
-    border: 1px solid ${ADMIN_COLOR.border_dark_grey};
-  }
-  font-size: ${FONT_SIZE.sm};
-  color: ${COLOR.text_dark};
   font-weight: bold;
-  margin-left: 30px;
-  margin-top: 2px;
-  padding: 5px;
-  white-space: pre-wrap;
+  text-align: left;
+`
+const RequireMsg = styled.p`
+  margin-left: 10px;
+  margin-top: 3px;
+  color: ${ADMIN_COLOR.warning};
+  font-size: ${FONT_SIZE.xs};
+  font-weight: bold;
+  text-align: left;
 `
 
-function ProductTextAreaSet({ title, value }) {
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 10px 0px 50px 0px;
+`
+
+const ButtonDiv = styled.div`
+  width: 15%;
+  margin: 0px 8px;
+`
+function ButtonForImgForm({ onLeaveClick }) {
   return (
-    <FormInput>
-      <FormInputTitle title={title} />
-      <ProductDescTextArea rows='5' cols='69' value={value} />
-    </FormInput>
+    <ButtonContainer>
+      <ButtonDiv onClick={onLeaveClick}>
+        <LogoutBtn color='admin_blue'>離開</LogoutBtn>
+      </ButtonDiv>
+    </ButtonContainer>
   )
 }
 
-function ProductInputSet({ title, value, className, name, error, onChange }) {
+function ButtonGroup({
+  status,
+  onEditClick,
+  onSaveClick,
+  onLeaveClick,
+  isValid
+}) {
   return (
-    <FormInput>
-      <FormInputTitle title={title} />
-      <FormInputMsgDiv>
-        <FormInputContent
-          value={value}
-          onChange={onChange}
-          className={className}
-          name={name}
-          type='text'
-        />
-        {error && <ErrorMsgStyle>請填入此欄位</ErrorMsgStyle>}
-      </FormInputMsgDiv>
-    </FormInput>
+    <ButtonContainer>
+      {status === 'edit' ? (
+        <ButtonDiv onClick={onEditClick}>
+          <EditBtn color='admin_blue'>編輯</EditBtn>
+        </ButtonDiv>
+      ) : (
+        <ButtonDiv onClick={onSaveClick}>
+          <SaveBtn color={isValid ? 'admin_blue' : ''}>儲存</SaveBtn>
+        </ButtonDiv>
+      )}
+      <ButtonDiv onClick={onLeaveClick}>
+        <LogoutBtn color='admin_blue'>離開</LogoutBtn>
+      </ButtonDiv>
+    </ButtonContainer>
+  )
+}
+
+//InfoForm
+const DropdownStyle = styled.select`
+  width: 200px;
+  padding: 8px;
+  border: 1px solid ${ADMIN_COLOR.border_grey};
+  margin-left: 12px;
+  &:focus {
+    border: 1px solid ${ADMIN_COLOR.border_dark_grey};
+  }
+`
+
+function Dropdown({ onChange, dropdownTitle, valueArray }) {
+  return (
+    <DropdownStyle
+      name='filter'
+      id='filter'
+      defaultValue={'DEFAULT'}
+      onChange={onChange}
+    >
+      <option value='DEFAULT' disabled>
+        {dropdownTitle}
+      </option>
+      {valueArray.map((value) => {
+        return <option value={value}>{value}</option>
+      })}
+    </DropdownStyle>
   )
 }
 
 export {
-  FormContainer,
+  FormWrapper,
+  Form,
+  Input,
+  InputTitle,
+  ErrorMsg,
+  RequireMsg,
   FormTitle,
-  FormInputContainer,
-  ProductInputSet,
-  ProductTextAreaSet,
-  ErrorMsgStyle
+  FormTitleBorder,
+  Dropdown,
+  ButtonGroup,
+  ButtonForImgForm
 }
