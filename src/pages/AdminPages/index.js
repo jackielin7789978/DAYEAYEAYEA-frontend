50510ca81a27bb6c9a0bb7bd1fff0d35e85fa208
Ding <mumu892101@gmail.com>
2021-10-27 22:13:34 +0800

    yard add formik & fix path

--- src/pages/AdminPages/index.js
+++ src/pages/AdminPages/index.js
@@ -1,12 +1,17 @@
 import AdminLogin from './AdminLogin'
 import AdminMembers from './AdminMembers'
 import AdminOrders from './AdminOrders'
-import { AdminProducts, AdminProductDetail } from './AdminProducts'
+import {
+  AdminProducts,
+  AdminProductDetail,
+  AdminProductAdd
+} from './AdminProducts'
 
 export {
   AdminLogin,
   AdminMembers,
   AdminOrders,
   AdminProducts,
-  AdminProductDetail
+  AdminProductDetail,
+  AdminProductAdd
 }
