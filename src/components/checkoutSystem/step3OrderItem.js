7fb5135581b5d22fe35d2847573a97dba4866246
jingjia peng <misjsdes@gmail.com>
2021-11-01 17:45:49 +0800

    變更結帳頁商品圖陣列抓取條件

--- src/components/checkoutSystem/step3OrderItem.js
+++ src/components/checkoutSystem/step3OrderItem.js
@@ -12,8 +12,13 @@ import { formatPrice } from '../../utils'
 export const OrderItem = ({ item }) => {
   return (
     <Item key={item.productId}>
+      {/* 有些商品的 Product_imgs 陣列沒有 3 個(例如耳環)，一律抓第 3 個會出錯，故新增判斷 */}
       <ItemImg
-        img={item.Product.Product_imgs[0].imgUrlSm}
+        img={
+          item.Product.Product_imgs[2]?.imgUrlSm
+            ? item.Product.Product_imgs[2].imgUrlSm
+            : item.Product.Product_imgs[0].imgUrlSm
+        }
         to={`/products/${item.productId}`}
       />
       <ItemInfo>
